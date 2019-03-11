e76fed3b77e573d7aa4d8e49495499df4ac25284
kemnitzs <kemnitz.stefan@gmail.com>
2019-03-11 10:31:18 +0100

    added cpp vorlesung

--- slides/encrypt.sh
+++ slides/encrypt.sh
@@ -1,4 +1,4 @@
 mkdir encrypted
 for i in `ls *.pdf`; do
-  pdftk $i output encrypted/$i.encrypt.pdf owner_pw $2 user_pw $3
+  pdftk $i output encrypted/$i.encrypt.pdf owner_pw "$2" user_pw "$3"
 done
